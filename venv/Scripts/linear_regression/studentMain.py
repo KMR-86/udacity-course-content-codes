aeb728eb71a6925c2feabe250c833e0fe5305ba8
mushi1002 <mahmud.mushfique@gmail.com>
2019-11-11 01:13:58 +0600

    linear regression code added . But this code wont work as there is no data set given in udacity. it will only work in udacity environment

---
+++ venv/Scripts/linear_regression/studentMain.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import numpy
+import matplotlib
+matplotlib.use('agg')
+
+import matplotlib.pyplot as plt
+from studentRegression import studentReg
+from class_vis import prettyPicture, output_image
+
+from ages_net_worths import ageNetWorthData
+
+ages_train, ages_test, net_worths_train, net_worths_test = ageNetWorthData()
+#this code won't run because there is no ages_net_worths given in the codes
+#this code will only run on udacity environment
+
+
+
+reg = studentReg(ages_train, net_worths_train)
+
+
+plt.clf()
+plt.scatter(ages_train, net_worths_train, color="b", label="train data")
+plt.scatter(ages_test, net_worths_test, color="r", label="test data")
+plt.plot(ages_test, reg.predict(ages_test), color="black")
+plt.legend(loc=2)
+plt.xlabel("ages")
+plt.ylabel("net worths")
+
+
+plt.savefig("test.png")
+output_image("test.png", "png", open("test.png", "rb").read())
